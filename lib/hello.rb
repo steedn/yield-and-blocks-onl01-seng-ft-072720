dba728263695748e63874a9c5cc37f6f74550f9f
Nathan Steed <steedn@outlook.com>
2020-07-30 16:52:12 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,11 +5,7 @@ def hello_t(array)
     yield array[i]
     i += 1
   end
+  array
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
